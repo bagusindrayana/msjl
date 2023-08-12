67c496eeea7409c3a5ed7f1c85628f9e048c92d4
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2023-08-12 22:18:23 +0800

    migraiton berkas

---
+++ database/migrations/2023_08_12_140900_create_berkas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berkas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('nomor_berkas',50);
+            $table->string('asal_berkas',150);
+            $table->string('perihal',200);
+            $table->date('tanggal_masuk');
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('berkas');
+    }
+};
